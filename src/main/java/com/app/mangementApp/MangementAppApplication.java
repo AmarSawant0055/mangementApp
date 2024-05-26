5d0d7f51bf0325b9f448bc4ebd9a27379bcd212c
amar sawant <amarsawant1232@gmail.com>
2024-05-26 23:08:28 +0530

    Initial commit

---
+++ src/main/java/com/app/mangementApp/MangementAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.mangementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/mangementApp/MangementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mangementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
